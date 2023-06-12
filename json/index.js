e3a77d764aae079b13d3451e60ab91b568e8283b
Zeev <87901027+Zeev-x@users.noreply.github.com>
2023-06-12 15:16:57 +0700

    Add files via upload

---
+++ json/index.js
@@ -0,0 +1,238 @@
+function azey(){
+  const x = new XMLHttpRequest();
+  const url = "https://zeev-x.github.io/js/json/azey.json";
+  const method = "GET";
+  
+  x.open(method,url,true);
+  x.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(x.responseText);
+      data.forEach(function(result){
+        var web = `<a href="${result.img}"><img src="${result.img}" height="200px"></a>`;
+        document.getElementById("result").innerHTML += web;
+      });
+    }
+  };
+  x.send();
+}
+function kiwora(){
+  const x = new XMLHttpRequest();
+  const url = "https://zeev-x.github.io/js/json/kiwora.json";
+  const method = "GET";
+  
+  x.open(method,url,true);
+  x.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(x.responseText);
+      data.forEach(function(result){
+        var web = `<a href="${result.img}"><img src="${result.img}" height="200px"></a>`;
+        document.getElementById("result").innerHTML += web;
+      });
+    }
+  };
+  x.send();
+}
+function albedo(){
+  const x = new XMLHttpRequest();
+  const url = "https://zeev-x.github.io/js/json/albedo.json";
+  const method = "GET";
+  
+  x.open(method,url,true);
+  x.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(x.responseText);
+      data.forEach(function(result){
+        var web = `<a href="${result.img}"><img src="${result.img}" height="200px"></a>`;
+        document.getElementById("result").innerHTML += web;
+      });
+    }
+  };
+  x.send();
+}
+function cosplay(){
+  const x = new XMLHttpRequest();
+  const url = "https://zeev-x.github.io/js/json/cosplay.json";
+  const method = "GET";
+  
+  x.open(method,url,true);
+  x.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(x.responseText);
+      data.forEach(function(result){
+        var web = `<a href="${result.img}"><img src="${result.img}" height="200px"></a>`;
+        document.getElementById("result").innerHTML += web;
+      });
+    }
+  };
+  x.send();
+}
+function ecchi(){
+  const x = new XMLHttpRequest();
+  const url = "https://zeev-x.github.io/js/json/ecchi.json";
+  const method = "GET";
+  
+  x.open(method,url,true);
+  x.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(x.responseText);
+      data.forEach(function(result){
+        var web = `<a href="${result.img}"><img src="${result.img}" height="200px"></a>`;
+        document.getElementById("result").innerHTML += web;
+      });
+    }
+  };
+  x.send();
+}
+function houshou(){
+  const x = new XMLHttpRequest();
+  const url = "https://zeev-x.github.io/js/json/houshou.json";
+  const method = "GET";
+  
+  x.open(method,url,true);
+  x.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(x.responseText);
+      data.forEach(function(result){
+        var web = `<a href="${result.img}"><img src="${result.img}" height="200px"></a>`;
+        document.getElementById("result").innerHTML += web;
+      });
+    }
+  };
+  x.send();
+}
+function loli(){
+  const x = new XMLHttpRequest();
+  const url = "https://zeev-x.github.io/js/json/loli.json";
+  const method = "GET";
+  
+  x.open(method,url,true);
+  x.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(x.responseText);
+      data.forEach(function(result){
+        var web = `<a href="${result.img}"><img src="${result.img}" height="200px"></a>`;
+        document.getElementById("result").innerHTML += web;
+      });
+    }
+  };
+  x.send();
+}
+function miku(){
+  const x = new XMLHttpRequest();
+  const url = "https://zeev-x.github.io/js/json/miku.json";
+  const method = "GET";
+  
+  x.open(method,url,true);
+  x.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(x.responseText);
+      data.forEach(function(result){
+        var web = `<a href="${result.img}"><img src="${result.img}" height="200px"></a>`;
+        document.getElementById("result").innerHTML += web;
+      });
+    }
+  };
+  x.send();
+}
+function neko(){
+  const x = new XMLHttpRequest();
+  const url = "https://zeev-x.github.io/js/json/neko.json";
+  const method = "GET";
+  
+  x.open(method,url,true);
+  x.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(x.responseText);
+      data.forEach(function(result){
+        var web = `<a href="${result.img}"><img src="${result.img}" height="200px"></a>`;
+        document.getElementById("result").innerHTML += web;
+      });
+    }
+  };
+  x.send();
+}
+function nino(){
+  const x = new XMLHttpRequest();
+  const url = "https://zeev-x.github.io/js/json/nino.json";
+  const method = "GET";
+  
+  x.open(method,url,true);
+  x.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(x.responseText);
+      data.forEach(function(result){
+        var web = `<a href="${result.img}"><img src="${result.img}" height="200px"></a>`;
+        document.getElementById("result").innerHTML += web;
+      });
+    }
+  };
+  x.send();
+}
+function yor(){
+  const x = new XMLHttpRequest();
+  const url = "https://zeev-x.github.io/js/json/yor.json";
+  const method = "GET";
+  
+  x.open(method,url,true);
+  x.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(x.responseText);
+      data.forEach(function(result){
+        var web = `<a href="${result.img}"><img src="${result.img}" height="200px"></a>`;
+        document.getElementById("result").innerHTML += web;
+      });
+    }
+  };
+  x.send();
+}
+function yumeko(){
+  const x = new XMLHttpRequest();
+  const url = "https://zeev-x.github.io/js/json/yumeko.json";
+  const method = "GET";
+  
+  x.open(method,url,true);
+  x.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(x.responseText);
+      data.forEach(function(result){
+        var web = `<a href="${result.img}"><img src="${result.img}" height="200px"></a>`;
+        document.getElementById("result").innerHTML += web;
+      });
+    }
+  };
+  x.send();
+}
+function zero(){
+  const x = new XMLHttpRequest();
+  const url = "https://zeev-x.github.io/js/json/zero.json";
+  const method = "GET";
+  
+  x.open(method,url,true);
+  x.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(x.responseText);
+      data.forEach(function(result){
+        var web = `<a href="${result.img}"><img src="${result.img}" height="200px"></a>`;
+        document.getElementById("result").innerHTML += web;
+      });
+    }
+  };
+  x.send();
+}
+function gawr(){
+  const x = new XMLHttpRequest();
+  const url = "https://zeev-x.github.io/js/json/gawr.json";
+  const method = "GET";
+  
+  x.open(method,url,true);
+  x.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(x.responseText);
+      data.forEach(function(result){
+        var web = `<a href="${result.img}"><img src="${result.img}" height="200px"></a>`;
+        document.getElementById("result").innerHTML += web;
+      });
+    }
+  };
+  x.send();
+}

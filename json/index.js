29d56004c0e6a3c7e5e11024f7b1f4c0d466594a
Zeev <87901027+Zeev-x@users.noreply.github.com>
2023-06-17 13:11:07 +0700

    update index.js

--- json/index.js
+++ json/index.js
@@ -253,3 +253,20 @@ function gawr(){
   };
   x.send();
 }
+function stars(){
+  const x = new XMLHttpRequest();
+  const url = "https://zeev-x.github.io/js/json/stars.json";
+  const method = "GET";
+  
+  x.open(method,url,true);
+  x.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(x.responseText);
+      data.forEach(function(result){
+        var web = `<a href="${result.img}"><img src="${result.img}" height="200px"></a>`;
+        document.getElementById("result").innerHTML += web;
+      });
+    }
+  };
+  x.send();
+}

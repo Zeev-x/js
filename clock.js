b8ade8a3b25b77f1c19e993b51a8279ddb394041
Zeev <87901027+Zeev-x@users.noreply.github.com>
2023-07-20 10:13:34 +0700

    update clock.js

--- clock.js
+++ clock.js
@@ -1,1 +1,12 @@
-
+const clock = setInterval(function(){
+  const time = new Date();
+  if(time){
+    var hour = time.getHours();
+    var minute = time.getMinutes();
+    var second = time.getSeconds();
+    
+    document.getElementById("hour").innerHTML = hour;
+    document.getElementById("minute").innerHTML = minute;
+    document.getElementById("second").innerHTML = second;
+  }
+},1000);

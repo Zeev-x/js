d3e74e7cd3ee5c28cbb657a5fbf9664cba8a5ba7
Zeevalya <87901027+Zeev-x@users.noreply.github.com>
2023-07-23 08:23:04 +0700

    update chat.js

--- chat.js
+++ chat.js
@@ -1,1 +1,27 @@
+const app = new XMLHttpRequest();
 
+function view(){
+  const send = document.getElementById("view");
+  app.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      send.innerHTML = this.responseText;
+    }
+  };
+  app.open("GET","./session.txt",true);
+  app.send();
+}
+
+window.onload = view();
+setInterval(function(){view()},1000);
+
+function chat(){
+  const text = document.getElementById("message").value;
+  app.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      this.responseText;
+    }
+  };
+  app.open("POST","./chat.php",true);
+  app.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+  app.send("data="+text);
+}
